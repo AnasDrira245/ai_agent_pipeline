2926284b805b1d98721d26e113d2eb97471e5159
Anas Drira <anas.drira2011@gmail.com>
2024-07-06 23:17:52 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.environ["MISTRAL_API_KEY"]
+
+st.title("💬 Chatbot")
+model = "mistral-large-latest"
+st.write(
+    "My name is Anas Drira This is a simple chatbot that uses Mistreal model to generate responses."
+    "this webpage is used for testing now "
+)
+
+# Ask user for their OpenAI API key via `st.text_input`.
+# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+
+
+if not api_key:
+    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+else:
+
+    client = MistralClient(api_key=api_key)
+
+    st.title("code generator")
+
+    # Set a default model
+    if "mistral_model" not in st.session_state:
+        st.session_state["mistral_model"] = model
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("What is up?"):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Get completion from Mistral
+        chat_response = client.chat(
+            model=st.session_state["mistral_model"],
+            messages=[
+                ChatMessage(role="user", content=prompt)
+            ]
+        )
+        assistant_message = chat_response.choices[0].message.content
+
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(assistant_message)
+
+        # Add assistant message to chat history
+        st.session_state.messages.append({"role": "assistant", "content": assistant_message})
