6f0e7bac37496c457754989708b5b89cd7f6a7c8
Anas Drira <91825085+AnasDrira245@users.noreply.github.com>
2024-07-12 20:48:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,62 +3,42 @@ from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
 from dotenv import load_dotenv
 import os
-
-load_dotenv()
-
-api_key = os.environ["MISTRAL_API_KEY"]
+from main2 import agent
 
 st.title("💬 Chatbot")
-model = "mistral-large-latest"
 st.write(
     "My name is Anas Drira i made this simple chatbot that uses Mistreal model to generate code."
     "this webpage is used for testing now "
 )
 
+st.title("code generator")
 
 
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-if not api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    client = MistralClient(api_key=api_key)
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-    st.title("code generator")
+    # Get completion from Mistral
+    try:
+        assistant_message = agent.chat(prompt)
+    except :
+        assistant_message="i'm not able to find answer to your query "
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(assistant_message)
 
-    # Set a default model
-    if "mistral_model" not in st.session_state:
-        st.session_state["mistral_model"] = model
-
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Accept user input
-    if prompt := st.chat_input("What is up?"):
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        # Display user message in chat message container
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Get completion from Mistral
-        chat_response = client.chat(
-            model=st.session_state["mistral_model"],
-            messages=[
-                ChatMessage(role="user", content=prompt)
-            ]
-        )
-        assistant_message = chat_response.choices[0].message.content
-
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            st.markdown(assistant_message)
-
-        # Add assistant message to chat history
-        st.session_state.messages.append({"role": "assistant", "content": assistant_message})
+    # Add assistant message to chat history
+    st.session_state.messages.append({"role": "assistant", "content": assistant_message})
